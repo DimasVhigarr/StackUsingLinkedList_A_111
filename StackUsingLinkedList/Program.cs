5873d307b2acd720716c40f148915d9340cbf742
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-11-30 15:49:18 +0700

    enambah teks

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -64,11 +64,21 @@ namespace StackUsingLinkedList
                 Console.WriteLine();
             }
         }
-    }
-    internal class Program
-    {
         static void Main(string[] args)
         {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push ");
+                Console.WriteLine("2. Pop ");
+                Console.WriteLine("3. Display ");
+                Console.WriteLine("4. Exit ");
+                Console.WriteLine("\nEnter Your Choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+            }
         }
     }
 }
